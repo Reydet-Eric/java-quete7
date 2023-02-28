f89d3072b64a28be46ad5a5d84114791cc2107fb
Reydet-Eric <eric.reydet@sncf.fr>
2023-02-28 11:46:19 +0100

    first commit

---
+++ Hangar.java
@@ -0,0 +1,11 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+
+    Car scenic = new Car ("Grand  Scenic");
+    Boat rafiot = new Boat ("La bière flottante");
+
+    System.out.println(scenic.doStuff());
+    System.out.println(rafiot.doStuff());
+    }
+}
